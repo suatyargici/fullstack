38a12cf5ec541c14c5410cf000cb3d294ebc0099
Suat-17 <yargicisuat@gmail.com>
2023-04-20 06:52:25 +0300

    chore: last change

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div
+      className="d-flex justify-content-center"
+      style={{ marginTop: "20px" }}
+    >
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-header">
+            <h3 className="text-center">Giriş Yap</h3>
+          </div>
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="form-group mt-2">
+                <label htmlFor="password">Şifre</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Şifre"
+                />
+              </div>
+              <div className="form-group">
+              <button type="submit" className="btn btn-primary w-100 mt-2">
+                Giriş Yap
+              </button>
+              </div>
+           
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
