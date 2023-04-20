ba3561c3f3c8685d46804493e9133fdd78fb7f1b
Suat-17 <yargicisuat@gmail.com>
2023-04-20 06:25:51 +0300

    chore: added react router

---
+++ frontend/src/pages/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Layout from "./layout";
+import Login from "./Login";
+import Register from "./Register";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
