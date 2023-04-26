a67dcf2c38e7b8b817ff501a8066c3be20c6e8f7
Suat-17 <yargicisuat@gmail.com>
2023-04-26 19:16:34 +0300

    chore: last change

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,74 @@
-const mongoose = require("mongoose")
-const express = require("express")
-const {v4:uuid} = require("uuid")
-const app = express()
-const multer =  require("multer")
-const cors = require("cors")
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+const uri =
+  "mongodb+srv://yargicisuat:Zgxgx3XkpHZSYGzb@sosyalmedya.hkzebvk.mongodb.net/test";
 
-app.listen(5000,()=>console.log("Server started on port 5000"))
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err.message));
 
+const userSchema = new mongoose.Schema({
+  _id: String,
+  name: String,
+  email: String,
+  password: String,
+  avatar: Object,
+});
+const User = mongoose.model("user", userSchema);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const secretKey = "Secret key secret key 12345";
+const options = {
+  expiresIn: "1h",
+};
+
+app.post("/api/register", upload.single("avatar"), async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({
+      _id: uuidv4(),
+      name,
+      email,
+      password,
+      avatar: req.file,
+    });
+
+    const result = await user.save();
+    const payload = {
+      user: result,
+    };
+    const token = {
+      token: jwt.sign(payload, secretKey, options),
+    };
+    res.json({
+      token: token,
+      user: result,
+    });
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.listen(5000, () => console.log("Server started on port 5000"));
